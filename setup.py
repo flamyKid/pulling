8e2db65d5ec4e9f41c87fb584679af9d5158731b
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-09-15 17:32:31 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pulling',
+      version='0.2',
+      packages=['pulling'],
+      license='Apache License 2.0',
+      author='ItYaS',
+      author_email='ryaboshapkoseraph@gmail.com',
+      url='https://github.com/ItYaS/pulling',
+      python_requires=">=3.5.1",
+      zip_safe=False)
