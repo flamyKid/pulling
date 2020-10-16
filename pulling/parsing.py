c64e45de89142220d3bec4ff4ccad5171d313c25
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-10-16 09:25:29 +0700

    добавление парсинга аудио и видео ссылок

--- pulling/parsing.py
+++ pulling/parsing.py
@@ -7,6 +7,9 @@ def get_text(url):
     # возвращаемый список с текстом
     text_list = list()
 
+    img_list = list()
+    video_list = list()
+    audio_list = list()
     href_dict = dict()  # словарь с ссылками и тегами <a> и <img>
 
     r = requests.get(url)
@@ -30,15 +33,42 @@ def get_text(url):
         if tag.string:
             href_dict[tag.string] = tag.get('href')
 
+    # ВЫТАСКИВАНИЕ ТЕКСТА ИЗ ТЕГОВ <audio>
+    for tag in soup.findAll('audio'):
+        if tag:
+            audio = BeautifulSoup(str(tag), 'html.parser')  # что бы искал внутри тега
+            if tag.get('src'):
+                audio_list.append(tag.get('src'))
+            for source in audio.findAll('source'):  # для тега <source>
+                if source and source.get('src'):
+                    audio_list.append(source.get('src'))
+
+    # ВЫТАСКИВАНИЕ ТЕКСТА ИЗ ТЕГОВ <video>
+    for tag in soup.findAll('video'):
+        if tag:
+            video = BeautifulSoup(str(tag), 'html.parser')  # что бы искал внутри тега
+            if tag.get('src'):
+                video_list.append(tag.get('src'))
+            for source in video.findAll('source'):   # для тега <source>
+                if source and source.get('src'):
+                    video_list.append(source.get('src'))
+
     # ВЫТАСКИВАНИЕ ТЕКСТА ИЗ ТЕГОВ <img>
     for img in soup.find_all('img'):
         try:
             alt = img.attrs['alt']
-        except KeyError:
-            continue
-        else:
-            href_dict[alt] = img.attrs['src']
+            if alt == '':  # для таких тегов как alt=""
+                img_list.append(img.get('src'))
+        except KeyError:  # на случай, если alt не существует
+            img_list.append(img.get('src'))
+        else:  # если существует
+            href_dict[alt] = img.get('src')
 
+    href_dict['img'] = img_list
+    href_dict['audio'] = audio_list
+    href_dict['video'] = video_list
+
+    # ВЫТАСКИВАНИЕ ТЕКСТА ИЗ ТЕГОВ <span>
     for span in soup.find_all('span'):
         text_list.append(span.string)
 
@@ -67,3 +97,4 @@ def find_text(url, pattern_list):
                     match_dict[tag] = href
 
     return match_dict
+
