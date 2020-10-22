24894e1b809e3d0e40e4fe62970f36e1752b4e4d
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-10-22 20:59:49 +0700

    улучшена читаемость

--- pulling/parsing.py
+++ pulling/parsing.py
@@ -7,9 +7,9 @@ def get_text(url):
     # возвращаемый список с текстом
     text_list = list()
 
-    img_list = list()
-    video_list = list()
-    audio_list = list()
+    img_src_list = list()
+    video_src_list = list()
+    audio_src_list = list()
     href_dict = dict()  # словарь с ссылками
 
     tags_list = ['p', 'h', 'b', 'big', 'small', 'i', 'strong', 'sub', 'sup', 'ins', 'del', 'th']  # теги, содержащие текст
@@ -41,35 +41,35 @@ def get_text(url):
         if tag:
             audio_tag = BeautifulSoup(str(tag), 'html.parser')  # что бы искал внутри тега
             if tag.get('src'):
-                audio_list.append(tag.get('src'))
+                audio_src_list.append(tag.get('src'))
             for source in audio_tag.findAll('source'):  # для тега <source>
                 if source and source.get('src'):
-                    audio_list.append(source.get('src'))
+                    audio_src_list.append(source.get('src'))
 
     # ВЫТАСКИВАНИЕ ТЕКСТА ИЗ ТЕГОВ <video>
     for tag in soup.findAll('video'):
         if tag:
             video_tag = BeautifulSoup(str(tag), 'html.parser')  # что бы искал внутри тега
             if tag.get('src'):
-                video_list.append(tag.get('src'))
+                video_src_list.append(tag.get('src'))
             for source in video_tag.findAll('source'):   # для тега <source>
                 if source and source.get('src'):
-                    video_list.append(source.get('src'))
+                    video_src_list.append(source.get('src'))
 
     # ВЫТАСКИВАНИЕ ТЕКСТА ИЗ ТЕГОВ <img>
     for img in soup.find_all('img'):
         try:
             alt = img.attrs['alt']
             if alt == '':  # для таких тегов как alt=""
-                img_list.append(img.get('src'))
+                img_src_list.append(img.get('src'))
         except KeyError:  # на случай, если alt не существует
-            img_list.append(img.get('src'))
+            img_src_list.append(img.get('src'))
         else:  # если существует
             href_dict[alt] = img.get('src')
 
-    href_dict['img'] = img_list
-    href_dict['audio'] = audio_list
-    href_dict['video'] = video_list
+    href_dict['img'] = img_src_list
+    href_dict['audio'] = audio_src_list
+    href_dict['video'] = video_src_list
 
     # ВЫТАСКИВАНИЕ ТЕКСТА ИЗ ТЕГОВ <span>
     for span in soup.find_all('span'):
