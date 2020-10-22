386fad88f62a8ae50da0773ad15ee53b327f8ef3
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-10-22 19:41:37 +0700

    добавление версии и atexit

--- pulling/__init__.py
+++ pulling/__init__.py
@@ -1,2 +1,12 @@
+import atexit
+
+
 __all__ = ['txt_format', 'rtf_format', 'pdf_format', 'docx_format',
            'avro_format', 'csv_format', 'json_format', 'parsing']
+
+__version__ = '1.2'
+
+
+@atexit.register
+def goodbye():
+    print('I hope we will meet soon!')
