a40eabcf4107a0d8157f7a60b55afdeebbb6081f
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-12-05 15:09:20 +0700

    изменение на классы

--- pulling/docx_format.py
+++ pulling/docx_format.py
@@ -1,52 +1,64 @@
 import re
-import zipfile
-
 try:
     from xml.etree.cElementTree import XML
 except ImportError:
     from xml.etree.ElementTree import XML
+import zipfile
 
 
-def get_text(path):
-    # возвращаемый список с текстом
-    text_list = list()
+class DOCX:
 
-    word_namespace = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
-    para = word_namespace + 'p'
-    text = word_namespace + 't'
+    def get_text(self, path):
+        # возвращаемый список с текстом
+        text_list = list()
 
-    document = zipfile.ZipFile(path)
-    xml_content = document.read('word/document.xml')
-    document.close()
-    tree = XML(xml_content)
+        word_namespace = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+        para = word_namespace + 'p'
+        text = word_namespace + 't'
 
-    raw_text_list = list()
-    for paragraph in tree.iter(para):
-        texts = [node.text for node in paragraph.iter(text) if node.text]
-        if texts:
-            raw_text_list.append(''.join(texts))
+        document = zipfile.ZipFile(path)
+        xml_content = document.read('word/document.xml')
+        document.close()
+        tree = XML(xml_content)
 
-    all_text = '\n\n'.join(raw_text_list)  # весь текст в одной строке
+        raw_text_list = list()
+        for paragraph in tree.iter(para):
+            texts = [node.text for node in paragraph.iter(text) if node.text]
+            if texts:
+                raw_text_list.append(''.join(texts))
 
-    strings = all_text.split('. ')  # разбиение по предложениям
-    for line in strings:
-        lines = line.split('\n')  # разбиение по абацам
-        for string in lines:
-            text_list.append(string)
+        all_text = '\n\n'.join(raw_text_list)  # весь текст в одной строке
 
-    return text_list
+        strings = all_text.split('. ')  # разбиение по предложениям
+        for line in strings:
+            lines = line.split('\n')  # разбиение по абацам
+            for string in lines:
+                text_list.append(string)
+
+        return text_list
+
+    def find_text(self, path, pattern_list):
+        # возвращаемый словарь с совпадениями
+        match_dict = dict()
+
+        # получение текста из файла
+        text_list = self.get_text(path)
+
+        for string in text_list:
+            for pattern in pattern_list:
+                match = re.search(pattern.lower(), string.lower())
+                if match:
+                    match_dict[string] = pattern
+
+        return match_dict
 
 
-def find_text(path, pattern_list):
-    # возвращаемый словарь с совпадениями
-    match_dict = dict()
+if __name__ == '__main__':
+    path = 'test/test.docx'
+    docx = DOCX()
 
-    text_list = get_text(path)
+    result_text = docx.get_text(path)
+    print(result_text)
 
-    for string in text_list:
-        for pattern in pattern_list:
-            match = re.search(pattern.lower(), string.lower())
-            if match:
-                match_dict[string] = pattern
-
-    return match_dict
+    found_text = docx.find_text(path, ['file'])
+    print(found_text)
