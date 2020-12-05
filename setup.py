9b7180e0cbd7f6466fd0bf76c252b1357819ef48
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-12-05 21:36:22 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pulling',
-      version='1.1',
+      version='1.2',
       packages=['pulling'],
       license='Apache License 2.0',
       author='ItYaS',
