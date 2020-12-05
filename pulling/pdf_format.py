348fcf99e9a2fe53525cba16e58efd5e96592596
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-12-05 15:11:14 +0700

    изменение на классы

--- pulling/pdf_format.py
+++ pulling/pdf_format.py
@@ -6,40 +6,54 @@ from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 
 
-def get_text(path):
-    # возвращаемый список с текстом
-    text_list = list()
+class PDF:
 
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle)
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    def get_text(self, path):
+        # возвращаемый список с текстом
+        text_list = list()
 
-    with open(path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
-            page_interpreter.process_page(page)
+        resource_manager = PDFResourceManager()
+        fake_file_handle = io.StringIO()
+        converter = TextConverter(resource_manager, fake_file_handle)
+        page_interpreter = PDFPageInterpreter(resource_manager, converter)
 
-        text = fake_file_handle.getvalue()
+        with open(path, 'rb') as fh:
+            for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+                page_interpreter.process_page(page)
 
-    converter.close()
-    fake_file_handle.close()
+            text = fake_file_handle.getvalue()
 
-    if text:
-        text_list = text.split('. ')
+        converter.close()
+        fake_file_handle.close()
 
-    return text_list
+        if text:
+            text_list = text.split('. ')
+
+        return text_list
+
+    def find_text(self, path, pattern_list):
+        # возвращаемый словарь с совпадениями
+        match_dict = dict()
+
+        # получение текста из файла
+        text_list = self.get_text(path)
+
+        for string in text_list:
+            for pattern in pattern_list:
+                match = re.search(pattern.lower(), string.lower())
+                if match:
+                    match_dict[string] = pattern
+
+        return match_dict
 
 
-def find_text(path, pattern_list):
-    # возвращаемый словарь с совпадениями
-    match_dict = dict()
+if __name__ == '__main__':
+    path = 'C:\\Users\\Samsung\\Desktop\\игры\\DnD\\книги\\Книга Игрока.pdf'
+    pdf = PDF()
 
-    text_list = get_text(path)
+    result_text = pdf.get_text(path)
+    print(result_text)
 
-    for string in text_list:
-        for pattern in pattern_list:
-            match = re.search(pattern.lower(), string.lower())
-            if match:
-                match_dict[string] = pattern
-
-    return match_dict
+    found_text = pdf.find_text(path, ['Опыт'])
+    for elem in found_text:
+        print(elem)
