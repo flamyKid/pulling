9f406030e84f378e4b972d73bc2986dc617958e9
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-12-09 17:18:25 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+import atexit
+
+
+__all__ = ['Txt', 'Rtf', 'Pdf', 'Docx', 'Csv', 'Avro', 'Json']
+
+__version__ = '1.4'
+
+
+@atexit.register
+def goodbye():
+    print('I hope we will meet soon!')
