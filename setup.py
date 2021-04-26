df8d4b408f8369ecf391d46ef6d737b9a25a3719
Серафим Рябошапко <70561906+ityas@users.noreply.github.com>
2021-04-26 17:35:53 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from os.path import join, dirname
 
 setup(name='pulling',
-      version='1.4.1',
+      version='1.4.2',
       packages=['pulling'],
       license='Apache License 2.0',
       author='ItYaS',
       author_email='ryaboshapkoseraph@gmail.com',
-      url='https://github.com/ItYaS/pulling',
+      url='https://github.com/ityas/pulling',
       description='Pulling is an open source python repository for working with files of different extensions.',
       long_description=open(join(dirname(__file__), 'README.txt')).read(),
       zip_safe=False)
